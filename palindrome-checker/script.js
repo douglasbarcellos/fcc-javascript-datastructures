923508518157394285e8b66b20ddbf1c5f881076
Douglas Barcellos <douglasbarcellosti@gmail.com>
2022-11-20 21:50:34 -0300

    JS - Algorithms

---
+++ palindrome-checker/script.js
@@ -0,0 +1,8 @@
+//Palindrome Checker: >>>
+function palindrome(str) {
+    let str1 = str.toLowerCase().replace(/[\W_]/g, '');
+    let str2 = str1.split('').reverse().join('');
+    return str1 === str2 ? true : false;}
+  palindrome("eye");
+
+  
---
+++ roman-numeral-converter/script.js
@@ -0,0 +1,14 @@
+//Roman Numeral Converter: >>>
+function convertToRoman(num) {
+    let result = '';
+    let romanNums = {
+      M: 1000, CM: 900, D: 500,
+      CD: 400, C: 100, XC: 90,
+      L: 50, XL: 40, X: 10,
+      IX: 9,  V: 5, IV: 4, I: 1  };
+    for (let val in romanNums) {
+      for (let i = romanNums[val]; num >= i; num -= i) result += val;  }
+    console.log(result);
+    return result;
+  }
+  convertToRoman(36);
